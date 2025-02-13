7c855a8e2d9c32b8045fc73637365d308c59e649
crL96 <190506813+crL96@users.noreply.github.com>
2025-02-13 11:58:19 +0100

    Add both iterative and recursive function for fibonacci sequence array

---
+++ fibonacci.js
@@ -0,0 +1,30 @@
+function fibs(n) {
+    const fibArray = [0, 1];
+
+    if (n <= 1) {
+        return [0];
+    }
+
+    for (let i = 2; i < n; i++) {
+        fibArray.push(fibArray[i-1] + fibArray[i-2])
+    }
+    return fibArray;
+}
+
+
+function fibsRec(n) {
+    if (n <= 0) {
+        return [];
+    }
+    if (n == 1) {
+        return [0]
+    }
+    if (n == 2) {
+        return [0, 1]
+    }
+    const prevArray = fibsRec(n - 1)
+    return [...prevArray, prevArray[n-2] + prevArray[n-3]]
+}
+
+
+console.log(fibsRec(5));
